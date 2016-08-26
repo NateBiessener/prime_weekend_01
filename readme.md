3c155032c21bf5b1b36ef3449cddce29187969fe
Nate Biessener <nathaniel.biessener@gmail.com>
2016-08-26 17:29:10 -0500

    v0.6_deleteButton

--- readme.md
+++ readme.md
@@ -4,8 +4,9 @@ Employee Tracker
 Plan
 * 0.1 source script X
 * 0.2 create form fields and pull input into script X
-* 0.3 add info to DOM 
-* 0.4 add total salary
-* 0.5 create delete button
-* 0.6 update salary total with delete button
+* 0.3 add info to DOM X
+* 0.4 add total salary X
+* 0.5 create delete button X
+* 0.6 update salary total with delete button X
+* 0.7 Make delete button display all matching employees and offer choice
 * 1.0 clean up the code
--- trackerScript.js
+++ trackerScript.js
@@ -2,13 +2,17 @@ console.log('script sourced');
 
 var employees = [];
 var addEmployee = function(){
-  employees.push([
-    document.getElementById('firstNameIn').value,
-    document.getElementById('lastNameIn').value,
-    document.getElementById('employeeIdIn').value,
-    document.getElementById('titleIn').value,
-    document.getElementById('salaryIn').value
-  ]);
+  employees.push({
+    firstName: document.getElementById('firstNameIn').value,
+    lastName: document.getElementById('lastNameIn').value,
+    empId: document.getElementById('employeeIdIn').value,
+    jobTitle: document.getElementById('titleIn').value,
+    salary: document.getElementById('salaryIn').value
+  });
+  displayEmployees();
+};
+
+var displayEmployees = function(){
   //clear form fields
   document.getElementById('firstNameIn').value = '';
   document.getElementById('lastNameIn').value = '';
@@ -22,30 +26,42 @@ var addEmployee = function(){
   for (var i = 0; i < employees.length; i++) {
     //create var for each employee for readability purposes
     var employee = employees[i];
-    for (var j = 0; j < employee.length; j++) {
-      //add each piece of employee to DOM with customized message
-      switch (j) {
-        case 0:
-          document.getElementById('display').innerHTML += '<p>First Name: ' + employee[j] + '</p>';
-          break;
-        case 1:
-          document.getElementById('display').innerHTML += '<p>Last Name: ' + employee[j] + '</p>';
-          break;
-        case 2:
-          document.getElementById('display').innerHTML += '<p>Employee ID: ' + employee[j] + '</p>';
-          break;
-        case 3:
-          document.getElementById('display').innerHTML += '<p>Job Title: ' + employee[j] + '</p>';
-          break;
-        case 4:
-          document.getElementById('display').innerHTML += '<p>Annual Salary: ' + employee[j] + '</p><br />';
-          break;
-        default:
-          console.log('In switch default. Time to debug');
-      }//end switch
-
-    }//end nested for
-    totalSalary += parseInt(employee[4]);
-  }//end outer for
+    //display employee data on DOM
+    document.getElementById('display').innerHTML += '<p>First Name: ' + employee.firstName + '</p>';
+    document.getElementById('display').innerHTML += '<p>Last Name: ' + employee.lastName + '</p>';
+    document.getElementById('display').innerHTML += '<p>Employee ID: ' + employee.empId + '</p>';
+    document.getElementById('display').innerHTML += '<p>Job Title: ' + employee.jobTitle + '</p>';
+    document.getElementById('display').innerHTML += '<p>Annual Salary: $' + employee.salary + '</p><br />';
+    totalSalary += parseInt(employee.salary);
+  }//end for
   document.getElementById('display').innerHTML += '<p>Total salary per month: $' + totalSalary/12 + '</p>';
 };//end addEmployee
+
+var deleteEmployee = function(){
+  //marshall variables
+  var firstName = document.getElementById('deleteFirstNameIn').value;
+  var lastName = document.getElementById('deleteLastNameIn').value;
+  //clear form fields
+  document.getElementById('deleteFirstNameIn').value = '';
+  document.getElementById('deleteLastNameIn').value = '';
+  //search for employee by lastName, confirm firstName matches, then remove employee from employees
+  var empMatches = [];
+  for (var i = 0; i < employees.length; i++) {
+    if (employees[i].lastName.toUpperCase() === lastName.toUpperCase()) {
+      if (employees[i].firstName.toUpperCase() === firstName.toUpperCase()) {
+        empMatches.push(employees[i]);
+      }//end inner if
+    }//end outer if
+  }//end for
+  switch (empMatches.length) {
+    case 0:
+      alert('No matching records. Please enter an existing employee name.')
+    case 1:
+      employees.splice(employees.indexOf(empMatches[0]), 1);
+      break;
+    default:
+      //add ability to choose which employee of multiple found to remove
+      console.log('More than one matching employee found. Feature coming soon.');
+  }
+  displayEmployees();
+};//end deleteEmployee
